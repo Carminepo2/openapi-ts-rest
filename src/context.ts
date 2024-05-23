9a014c6df69b6e3b3b7b635e20feeb28aaa92cac
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-05-23 11:19:21 +0200

    Moved `createDependencyGraph inside context file`

--- src/context.ts
+++ src/context.ts
@@ -9,8 +9,8 @@ import {
   type ResponseObject,
   type SchemaObject,
   isReferenceObject,
+  type ComponentsObject,
 } from "openapi3-ts/oas30";
-import { createDependencyGraph } from "./createDependencyGraph";
 import { formatToIdentifierString, topologicalSort } from "./lib/utils";
 
 const OPEN_API_COMPONENTS_PATH = ["schemas", "parameters", "requestBodies", "responses", "headers"] as const;
@@ -54,7 +54,7 @@ export function generateContext(openAPIDoc: OpenAPIObject) {
 
   const getSchemaByRef = getObjectByRef<SchemaObject>;
 
-  const graph = createDependencyGraph(openAPIDoc, getSchemaByRef);
+  const graph = createSchemaComponentsDependencyGraph(openAPIDoc.components?.schemas, getSchemaByRef);
   const topologicallySortedSchemaRefs = topologicalSort(graph);
 
   const schemasToExportMap = new Map<
@@ -124,4 +124,59 @@ function validateAndParseRef(ref: string): {
     componentPath,
     componentName,
   };
+}
+
+function createSchemaComponentsDependencyGraph(
+  schemaComponents: ComponentsObject["schemas"],
+  getSchemaByRef: (ref: string) => SchemaObject
+): Record<string, Set<string>> {
+  const graph: Record<string, Set<string>> = {};
+  const visitedRefs: Record<string, boolean> = {};
+
+  function visit(component: SchemaObject | ReferenceObject, fromRef: string): void {
+    if (isReferenceObject(component)) {
+      if (!(fromRef in graph)) {
+        graph[fromRef] = new Set();
+      }
+
+      graph[fromRef].add(component.$ref);
+
+      if (visitedRefs[component.$ref]) return;
+
+      visitedRefs[fromRef] = true;
+      visit(getSchemaByRef(component.$ref), component.$ref);
+      return;
+    }
+
+    (["allOf", "oneOf", "anyOf"] as const satisfies (keyof SchemaObject)[]).forEach((key) => {
+      component[key]?.forEach((subComponent) => {
+        visit(subComponent, fromRef);
+      });
+    });
+
+    if (component.type === "array" && component.items) {
+      visit(component.items, fromRef);
+      return;
+    }
+
+    if (component.type === "object" || component.properties || component.additionalProperties) {
+      if (component.properties) {
+        Object.values(component.properties).forEach((component) => {
+          visit(component, fromRef);
+        });
+      }
+
+      if (component.additionalProperties && typeof component.additionalProperties === "object") {
+        visit(component.additionalProperties, fromRef);
+      }
+    }
+  }
+
+  if (schemaComponents) {
+    Object.entries(schemaComponents).forEach(([name, schema]) => {
+      visit(schema, `#/components/schemas/${name}`);
+    });
+  }
+
+  return graph;
 }
