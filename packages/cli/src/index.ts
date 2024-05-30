85619a614293d7e28dac49a9cc43f11a3dae27ee
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-05-30 23:04:39 +0200

    Update index.ts

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -3,13 +3,16 @@ import { generateTsRestContractFromOpenAPI } from "@openapi-to-ts-rest/core";
 import { writeFileSync } from "fs";
 import prettier from "prettier";
 
+import packageJson from "../package.json";
+
 const program = new Command();
 
 program
+  .version(packageJson.version)
   .description("Generates a ts-rest contract from an OpenAPI schema.")
-  .requiredOption("-i, --input <input>", "The input OpenAPI schema file.")
+  .argument("<input>", "The input OpenAPI schema file.")
   .requiredOption("-o, --output <output>", "The output file.")
-  .action(async ({ input, output }) => {
+  .action(async (input, { output }) => {
     const prettierConfig = await prettier.resolveConfig("./");
 
     const result = await generateTsRestContractFromOpenAPI({
