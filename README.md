416b0d6ec0205e32504c5639fb7efaba080b178d
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-06-08 19:26:41 +0200

    Preparation for rist release

---
+++ README.md
@@ -0,0 +1,13 @@
+> [!WARNING]  
+> This project is still in development and is not ready for production use.
+> Use at your own risk.
+
+<p align="center">
+  <img src="./assets/images/openapi-ts-rest-contract-avatar-light.png" />
+</p>
+
+# OpenAPI to ts-rest Contract
+
+A simple tool to generate a [ts-rest](https://github.com/ts-rest/ts-rest) contract from an OpenAPI 3.0/3.1 specification.
+
+## 🚧 WIP 🚧
--- packages/core/src/lib/prettier.ts
+++ packages/core/src/lib/prettier.ts
@@ -1,14 +1,13 @@
 import type { Options } from "prettier";
 
+import { format } from "prettier";
 import * as parserTypescript from "prettier/parser-typescript";
-import * as prettierPluginEstree from "prettier/plugins/estree";
-import * as prettier from "prettier/standalone";
 
 export async function prettify(input: string, config?: Options | null): Promise<string> {
   try {
-    return prettier.format(input.trim(), {
+    return format(input.trim(), {
       parser: "typescript",
-      plugins: [parserTypescript, prettierPluginEstree],
+      plugins: [parserTypescript],
       ...config,
     });
   } catch (error) {
