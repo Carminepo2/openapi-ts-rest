e8e40657f1d90a04205eb6de2da2a213fa6ec006
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-06-11 00:16:36 +0200

    Update prettier.ts

--- packages/core/src/lib/prettier.ts
+++ packages/core/src/lib/prettier.ts
@@ -1,15 +1,15 @@
 import type { Options } from "prettier";
 
-import parserTypescript from "prettier/parser-typescript";
-import prettierPluginEstree from "prettier/plugins/estree";
-import { format } from "prettier/standalone";
+import { format } from "prettier";
+import * as parserTypescript from "prettier/parser-typescript";
+import * as prettierPluginEstree from "prettier/plugins/estree";
 
 export async function prettify(input: string, config?: Options | null): Promise<string> {
   try {
     return format(input.trim(), {
       parser: "typescript",
-      plugins: [parserTypescript, prettierPluginEstree],
       ...config,
+      plugins: [parserTypescript, prettierPluginEstree],
     });
   } catch (error) {
     return input;
